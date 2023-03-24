ff577399c0428ba197b8bc6a20f31a40e39dd87f
Vinoth IT <vinothalljavacourses@gmail.com>
2023-03-23 21:47:27 -0400

    first webapp commit

---
+++ src/test/java/com/vinothit/web_app/AppTest.java
@@ -0,0 +1,20 @@
+package com.vinothit.web_app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
